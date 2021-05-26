17180eecfcf6b82a5e6d3085bcd338f3e716af74
Bhavik <pate3216@sheridancollege.ca>
2021-05-26 22:58:34 +0530

    first commit

---
+++ src/wordreverse/WordReverse.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wordreverse;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Bhavikkumar Patel
+ */
+public class WordReverse {
+
+    public static void main(String[] args) {
+        String name;
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter a string: ");
+        name = input.nextLine();
+
+        char[] revName = name.toCharArray();
+
+        for (int i = revName.length - 1; i >= 0; i--) {
+            System.out.print(revName[i]);
+        }
+    }
+
+}
